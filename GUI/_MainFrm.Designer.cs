55767e0f4612d60d4d87c8287a4096ed94cd213c
Quang Nguyễn Đăng <16521560@ms.uit.edu.vn>
2018-06-18 11:23:56 +0700

    Change name

--- GUI/_MainFrm.Designer.cs
+++ GUI/_MainFrm.Designer.cs
@@ -698,7 +698,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "_MainFrm";
-            this.Text = "Form1";
+            this.Text = "Easy Type";
             this.Load += new System.EventHandler(this._MainFrm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
